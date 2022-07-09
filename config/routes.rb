acde14c02740bed4f239d219950a9586197d9b64
brianspinos777 <brianspinos777@hotmail.com>
2022-07-08 19:49:47 -0700

    first commit, user: brianspinos777

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  devise_for :users
+  get 'static_pages/landing_page'
+  get 'static_pages/dashboard'
+  resources :users
+
+  root "static_pages#landing_page"
+
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
